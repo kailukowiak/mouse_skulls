5002c75b7c843335465b68cc9638f7a649e9a755
kaiserxc <kailukowiak@gmail.com>
2019-05-13 23:06:08 -0600

    Cleaned up code and started image viewer function

--- eda/eda.jl
+++ eda/eda.jl
@@ -1,20 +1,21 @@
+## Load libraries and nibabel
 using PyCall
 using Images
+using ImageView
+
 # ENV["PYTHON"] = "/home/kailukowiak/anaconda3/bin/python"
 # Useful code for setting the right python directory
 nib = pyimport("nibabel")
-
+## Load images
 img = nib.load("data/MouseSkulls/475.mnc")
 
 data = img.get_data()
 
+## Show random slices
 slice1 = data[300, :, :]
-(slice1)
 
 # (slice1)
 
-using TestImages, Images,
-using ImageView
 imshow(slice1)
 
 imshow(data)
@@ -25,3 +26,29 @@ data2 = img2.get_data()
 imshow(data2)
 
 imshow(data2[:, :, 225])
+
+"""
+takes `img::Aray` as an input and displays 3 images
+for each orientation.
+"""
+function image_viewer(img::Array)
+    shape = size(img)
+    lng = Int(round(shape[1]/2))
+    wdth = Int(round(shape[2]/2))
+    hght = Int(round(shape[3]/2))
+    imshow(img[lng, :, :])
+    imshow(img[:, wdth, :])
+    imshow(img[:, :, hght])
+
+end
+
+image_viewer(data)
+
+lng = Int(round(size(data)[1]/2))
+
+function test(data)
+    shape = size(data)
+    lng = Int(round(shape[1]/2))
+    imshow(data[lng, :, :])
+end
+test(data)
