ce126420fb9d9cedd5a886c36a79bc3109a6b310
Kai Lukowiak <kailukowiak@pop-os.localdomain>
2019-06-01 11:58:18 -0600

    Converted to function

--- TagParser.jl
+++ TagParser.jl
@@ -1,11 +1,18 @@
 using DelimitedFiles
-f = open("eda/data/MouseSkulls/1179_landmarks.tag") do file
-    read(file, String)
-end;
+##
+function tag_parser(f_name)
+    f = open("eda/data/MouseSkulls/1179_landmarks.tag") do file
+        read(file, String)
+    end;
+    f = split(f, "Points =\n ")[2]
 
-f = split(f, "Points =\n ")[2]
+    f = replace(f, " 0.1 1 1 \"Marker\"" => "");
+    f = replace(f, ";" => "");
 
-f = replace(f, " 0.1 1 1 \"Marker\"" => "");
-f = replace(f, ";" => "");
+    f = readdlm(IOBuffer(f));
+    f
 
-f = readdlm(IOBuffer(f));
+end
+
+##
+test = tag_parser("eda/data/MouseSkulls/1179_landmarks.tag")
