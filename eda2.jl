b8eb278ab1b5e76ed9e5a4bfd6507d954704eb07
Kai Lukowiak <kailukowiak@pop-os.localdomain>
2019-06-02 21:12:53 -0600

    Increased test to larger image

--- eda2.jl
+++ eda2.jl
@@ -1,24 +1,76 @@
-##
 using NIfTI
 using Images
 using ImageView
+nil = niread("data/bh_cc_bh0098_skull.nii")
+imshow(nil.raw)
 ##
 ni = niread("/home/kailukowiak/test.nii")
 
 # voxel_size is impotant for scaling the .tag files.
-voxel_size(ni.header)
+v_size = voxel_size(ni.header)
 
 imshow(ni.raw)
-
-
-ni.header
-
-ni.raw
-
 imshow(ni.raw[:, 200, :])
 
 # ~/PythonProjects/facial_key_points/eda/data/MouseSkulls/1179.mnc
 
+##
+ni_raw = ni.raw
+ni_raw .> 200
+imgg = Gray.(ni_raw)
+imgg = RGB.(imgg)
+##
+
 dim_info(ni.header)
 
-ni.header.dim .* 0.035 ./10
+loc = round.(Int, test ./ v_size[1])
+# ni.header.dim .* 0.035 ./10
+guidict = imshow(ni_raw)
+for i in 1:size(loc)[1]
+        annotate!(guidict,
+                AnnotationPoint(loc[i, 1],
+                loc[i, 2],
+                z = loc[i, 3],
+                color = RGB(1, 0, 0),
+                shape = '.'))
+
+end
+
+guidict = imshow(ni_raw)
+idx = annotate!(guidict, AnnotationPoint(370, 158,
+                z = 402,
+                color = RGB(1,0,0),
+                shape = '.'))
+
+idx = annotate!(guidict,
+                AnnotationPoint(loc[1, :][1],
+                                loc[1, :][2],
+                                z = loc[1, :][3],
+                                color = RGB(1, 0, 0),
+                                shape = '.'))
+
+
+imgg[loc[1, :]] = colorant"red"
+
+##
+
+
+using ImageView, GtkReactive, TestImages, Colors
+
+# Prepare the data
+mri = testimage("mri")
+mriseg = RGB.(mri)
+mriseg[mri .> 0.5] = colorant"red"
+
+mri .> 0.5
+##
+
+
+mri = testimage("mri")
+mriseg = RGB.(mri)
+mriseg[mri .> 0.5] = colorant"red"
+
+
+push!(LOAD_PATH, "/home/kailukowiak/DataScienceProjects/")
+
+LOAD_PATH
